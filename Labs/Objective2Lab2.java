14ed222fa178bc59655f93543fe8d4383f282049
Jacob <jwv928@gmail.com>
2023-02-16 17:54:21 -0500

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Jacob Vanderlaan";
+		int age = 28;
+		String state = "Nebraska";
+
+		System.out.println (name);
+		System.out.println (age);
+		System.out.println (state);
+
+
+	}
+}
