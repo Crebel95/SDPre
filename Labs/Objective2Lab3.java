8f4fee37be316521b2f1d5c1ac00e530492b1965
Jacob <jwv928@gmail.com>
2023-02-17 12:48:38 -0500

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+
+		int num_28 = 1995;
+		String num_01 = "January";
+		String num_2 = "False";
+
+		System.out.println (num_28);
+		System.out.println (num_01);
+		System.out.println (num_2);
+	}
+}
