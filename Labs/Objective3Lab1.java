e860662c732b5c00b828d5c44eb21866c94aee2b
Jacob <jwv928@gmail.com>
2023-02-17 20:28:13 -0500

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+
+		int num1 = 17;
+		int num2 = 6;
+		int num3 = 4;
+		
+		double sum = num1 + num2 + num3;
+
+		double average = sum / 3;
+
+		System.out.println (average);
+	}
+}
