f7effa6a117f76de73a06a04634a133748d0b1f0
Jacob <jwv928@gmail.com>
2023-03-01 15:07:14 -0500

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1 {
+	public static void main(String [] args) {
+		
+		int i = 1;
+		int n = 10;
+
+
+		for (i=1; i<=10; i+=1) {
+			System.out.println(i);
+		}
+		for(n=10; n>=1; n-=1){
+			System.out.println(n);
+		}
+
+	}
+}
