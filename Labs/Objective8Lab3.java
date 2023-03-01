150a62c8e3164deb74c96e4a25cd36c76957b29f
Jacob <jwv928@gmail.com>
2023-03-01 15:22:16 -0500

    Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,18 @@
+public class Objective8Lab3 {
+	public static void main(String [] args) {
+
+		int i = 1;
+
+		for(i=1; i<=20; i+=1) {
+
+			if(i % 2 == 1) {
+				System.out.println( i + " is odd");
+			}
+
+			else if(i % 2 == 0) {
+				System.out.println( i + " is even");
+			}
+
+		}
+	}
+}
